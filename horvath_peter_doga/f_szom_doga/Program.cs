6d29b658c078e0d2d113df7f88c19fb1e66f10bc
Trixep11 <82161045+Trixep@users.noreply.github.com>
2024-10-07 10:35:02 +0200

    Add files via upload

---
+++ horvath_peter_doga/f_szom_doga/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+
+namespace f_szom_doga
+{
+    internal class Program
+    {
+        public static List<Versenyzok> versenyzok = [];
+        static void Main(string[] args)
+        {
+            using StreamReader sr = new(
+                path: "forras.txt",
+                encoding: Encoding.UTF8);
+
+            while (!sr.EndOfStream) versenyzok.Add(new(sr.ReadLine()));
+            Console.WriteLine($"{versenyzok.Count} versenyző vett részt.");
+
+            Elit();
+            NoKor();
+            OsszKerekpar();
+            AtlagUszas();
+            FerfiGyoztes();
+            KategoriaBefejez();
+
+        }
+
+        private static void Elit()
+        {
+            var elit = versenyzok.FindAll(x => x.Kategoria == "elit").Count;
+            Console.WriteLine($"Ennyi versenyző van elit kategóriában: {elit}");
+        }
+
+        private static void NoKor()
+        {
+            var noKor = versenyzok.Where(x => x.Nem == false).Average(x => 2024 - x.SzulEv);
+            Console.WriteLine($"A nők átlag életkora: {noKor}");
+        }
+
+        private static void OsszKerekpar()
+        {
+            float ido = versenyzok.Sum(x => x.KerekparIdo);
+            ido = ido / 60 / 60;
+            Console.WriteLine($"A kerékpározással töltött idő: {Math.Round(ido, 2)} óra");
+        }
+
+        private static void AtlagUszas()
+        {
+            var ido = versenyzok.Where(x => x.Kategoria == "elit junior").Average(x => x.UszasIdo);
+            Console.WriteLine($"Az össz úszás idő elit juniorban idő: {ido / 60} perc");
+        }
+
+        private static void FerfiGyoztes()
+        {
+            var gyoztes = versenyzok.Where(x => x.Nem == true).Min(x => x.Time());
+            Console.WriteLine($"A férfi győztes: {gyoztes}");
+        }
+
+        private static void KategoriaBefejez()
+        {
+            string[] kategoriak = new string[]
+            {
+                "16 - 17",
+                "18 - 19",
+                "20 - 24",
+                "25 - 29",
+                "30 - 34",
+                "35 - 39",
+                "40 - 44",
+                "45 - 49",
+                "50 - 54",
+                "elit",
+                "elit junior"
+            };
+
+            for (int i = 0; i < kategoriak.Length; i++)
+            {
+                var ossz = versenyzok.FindAll(x => x.Kategoria == kategoriak[i]).Count;
+                Console.WriteLine($"{kategoriak[i]} kategória versenyszők száma: {ossz}");
+            }
+        }
+
+        private static void KategoriaDepo()
+        {
+
+        }
+    }
+}
---
+++ horvath_peter_doga/f_szom_doga/Versenyzok.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace f_szom_doga
+{
+    internal class Versenyzok
+    {
+        public string Nev { get; set; }
+        public int SzulEv { get; set; }
+        public int Rajtszam { get; set; }
+        public bool Nem { get; set; }
+        public string Kategoria { get; set; }
+        public int UszasIdo { get; set; }
+        public int Depo1 { get; set; }
+        public int KerekparIdo { get; set; }
+        public int Depo2 { get; set; }
+        public int FutasIdo { get; set; }
+
+        public override string ToString()
+        {
+            return $" {Nev} (Születési év: {SzulEv} " +
+                $"Rajtszám: {Rajtszam} perc " +
+                $"Nem: {Nem}" + 
+                $"Kategória: {Kategoria}" + 
+                $"Úszás ideje: {UszasIdo}" + 
+                $"Első depó ideje: {Depo1}" + 
+                $"Kerékpározás ideje: {KerekparIdo}" + 
+                $"Második depóban töltött idő: {Depo2}" + 
+                $"Futás ideje: {FutasIdo})";
+        }
+
+        public Versenyzok(string row)
+        {
+            var v = row.Split(';');
+            Nev = v[0];
+            SzulEv = int.Parse(v[1]);
+            Rajtszam = int.Parse(v[2]);
+            Nem = v[3] == "f" ? true : false; // férfi == true
+            Kategoria = v[4];
+            UszasIdo = ValtasMasodperc(v[5]);
+            Depo1 = ValtasMasodperc(v[6]);
+            KerekparIdo = ValtasMasodperc(v[7]);
+            Depo2 = ValtasMasodperc(v[8]);
+            FutasIdo = ValtasMasodperc(v[9]);
+        }
+
+        private int ValtasMasodperc(string time)
+        {
+            var v = time.Split(":");
+            int ora = int.Parse(v[0]);
+            int perc = int.Parse(v[1]);
+            int masodperc = int.Parse(v[2]);
+
+            return (ora * 60 * 60 + perc * 60 + masodperc);
+        }
+
+        public int Time()
+        {
+            int times = UszasIdo + FutasIdo + KerekparIdo + Depo1 + Depo2;
+            return times;
+        }
+    }
+}
